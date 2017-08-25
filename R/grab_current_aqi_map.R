6b1a81eefda92e0e545061e87cb5ea8804dd1297
dkvale <kvaled@gmail.com>
2017-08-25 09:05:11 -0500

    shorten current map dimensions

--- R/grab_current_aqi_map.R
+++ R/grab_current_aqi_map.R
@@ -14,10 +14,10 @@ webshot(map_url, file = file_path, zoom = 2)
 
 map <- image_read(file_path)
 
-map <- image_crop(map, "792x848+598+56")
+map <- image_crop(map, "794x766+598+90")
 #map2 <- image_crop(map, "400x428+296+26")
 
-#map
+map
 
 image_write(map, path = file_path, format = "png")
 
